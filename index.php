2bc470142e804734f37ea2bf2d0b3b38344d4a14
Ramis <i.ramis95@gmail.com>
2019-09-07 13:16:36 +0300

    dafasfa

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: irami
+ * Date: 01.06.2019
+ * Time: 22:43
+ */
+
+include 'application/lib/dev.php';
+
+use application\core\Router;
+
+spl_autoload_register(function($class) {
+    $path = str_replace('\\', '/', $class.'.php');
+
+    if (file_exists($path)) {
+        require $path;
+    }
+});
+
+session_start();
+
+$router = new Router();
+$router->run();
+
+
+
+
+
+
